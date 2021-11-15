b18c7b13bed70e20c3e6d02b9efe6a5172831f53
chwan62232 <erwanpresse.pro@gmail.com>
2021-11-15 15:46:09 +0100

    ACcueil

---
+++ Desktop/concours/footer.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body>
+    
+</body>
+</html>
+ 
+ <footer>
+
+ <div class="contenu-footer">
+
+<div class="bloc footer-reseaux">
+    <h3>👉Réseaux de la ville</h3>
+    <ul class="liste-reseaux">
+        <li><a href="https://fr-fr.facebook.com/villedelonguenesse/">Facebook</a></li>
+        <li><a href="https://www.linkedin.com/in/communication-longuenesse-2106ab18/?originalSubdomain=fr#">Linkedin</a></li>
+        <li><a href="https://www.instagram.com/mediatour.longuenesse/?hl=fr">Instagram</a></li>
+    </ul>
+</div>
+
+<div class="bloc footer-contact">
+    <h3>👉Contact</h3>
+    <ul class="liste-contact">
+        <li><a href="mailto:mairie@ville-longuenesse.fr">mairie@ville-longuenesse.fr</a></li>
+        <li>13, rue Joliot Curie , 62219 Longuenesse - FRANCE</li>
+        <li>+33321122300</li>
+    </ul>
+</div>
+
+<div class="bloc footer-apropos">
+    <h3>👉A propos</h3>
+    <ul class="liste-apropos">
+        <li><a href="mesenvies.html"> Mes envies</a></li>
+        <li><a href="mesvoyages.html"> Mes voyages</a></li>
+    </ul>
+</div>
+
+<div class="bloc footer-navigation">
+    <h3>👉Navigation</h3>
+    <ul class="liste-navigation">
+        <li><a href="capitales.html" target="blank">Les capitales les plus visités</a></li>
+        <li><a href="ameriquen.html">Amérique du nord</a></li>
+        <li><a href="asie.html">Asie</a></li>
+        <li><a href="europe.html">Europe</a></li>
+        <li><a href="afrique.html">Afrique</a></li>
+    </ul>
+</div>
+</div>
+
+
+
+
+
+</footer>

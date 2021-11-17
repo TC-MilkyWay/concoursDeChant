e33069918236dd7c8dfa37ed28a05dfeb2b127fc
chwan62232 <erwanpresse.pro@gmail.com>
2021-11-17 16:46:18 +0100

    ajout module connexion

--- connexion.php
+++ connexion.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Concours de chant - connexion</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-    
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Connexion</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
 </head>
-
 <body>
-
-    <?php include_once('header.php'); ?>
-    <div class="main">
-<p>eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee <br>eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</p>
+  <form class="connexion"> 
+     
+    <h1>Se connecter</h1>    
+    <div class="inputs">
+      <input type="email" placeholder="Pseudo" />
+      <input type="password" placeholder="Mot de passe">
     </div>
-    <?php include_once('footer.php'); ?>
-
+    
+    <div align="center">
+      <button type="submit">Se connecter</button>
+    </div>
+  </form>
 </body>
-
 </html>
+
+<?php include_once('header.php'); ?>
+<?php include_once('footer.php'); ?>

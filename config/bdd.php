861735e682af2d248b12c62bade62bcfef09d574
TC-MilkyWay <yohanngille@yahoo.co.uk>
2021-11-19 09:06:04 +0100

    test config

--- config/bdd.php
+++ config/bdd.php
@@ -2,9 +2,9 @@
 // Souvent on identifie cet objet par la variable $conn ou $db
 try{
 $mysqlConnection = new PDO(
-    'mysql:host=localhost;port=3307;dbname=concourschant;charset=utf8',
+    'mysql:host=localhost;port=3306;dbname=CONCOURS;charset=utf8',
     'root',
-    'root');
+    'password');
 }
 catch (Exeption $erreur)
 {
@@ -12,21 +12,21 @@ catch (Exeption $erreur)
 }
 /*our vérifier que vous ètes bien dans la base de donnée:
     si vous ne l'avez pas fait importer a partir de phpmyadmin le fichier du dossier sql
-    aller sur http://localhost/concoursDeChant/config/bdd.php
+    aller sur http://localhost:8080/config/bdd.php
     si vous voyer une page blanche c'est que vous êtes dans la base de données
     décommenter la partie ci dessous
     car On va maintenant tester si on arrive a récuperer une valeur de la table utilisateur*/
 
 
 
-// $sqlquery = 'SELECT * FROM utilisateur';
-// $usersStatement = $mysqlConnection->prepare($sqlquery);
-// $usersStatement->execute();
-// $users = $usersStatement->fetchall();
+$sqlquery = 'SELECT * FROM utilisateur';
+ $usersStatement = $mysqlConnection->prepare($sqlquery);
+ $usersStatement->execute();
+ $users = $usersStatement->fetchall();
 
 
-// foreach ($users as $user){
-// ?>
-    <p><?php //echo $user['NOM']; ?></p><?php
-// }
+ foreach ($users as $user){
+ ?>
+    <p><?php echo $user['NOM']; ?></p><?php
+ }
 ?>

4d8272df4f3f62d18d1eea91d09920f604843fdc
chwan62232 <erwanpresse.pro@gmail.com>
2021-11-25 10:17:43 +0100

    module de connexion avancé

--- connexion.php
+++ connexion.php
@@ -1,53 +1,37 @@
-<?php
-session_start();
-
+<?php 
 require  "config/bdds.php";
+$recherchemail = $mysqlConnection->prepare("SELECT * FROM utilisateur WHERE email = ?");
+$recherchemail->execute([$_POST['login']]);
+$utilisateur = $recherchemail->fetch();
 
-if(isset($_POST['formconnexion'])) {
-  $email = $_POST["login"];
-  $pass = $_POST["mdp"];
-  if(!empty($email)) {
-    $recherchemail = $mysqlConnection->prepare("SELECT * FROM utilisateur WHERE email= ? ");
-    $recherchemail ->execute(array($email));
-    $userexist = $recherchemail->rowCount();
-    $result = $recherchemail->fetchall();
-    echo $result;
-    if($userexist == 1) {
-     echo "concordance";
-      // $verif = password_verify($pass,$recherchemail );
-      // echo "le resultat de verif_password :".$verif;
-      // if ($verif){ 
-      // echo "GG";
-      // } else {
-      //   echo "Try again";
-      // }
-    }else{
-      echo " non concordance";
-    }
-  }
-} else {
-  $erreur = "Tous les champs doivent être complétés !";
+if ($utilisateur && password_verify($_POST['mdp'], $utilisateur['pass']))
+{
+  echo "valid";
+
+}else{
+  echo "invalid";
 }
-?>
+
+  ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Connexion</title>
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
-</head>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
+  </head>
 <body>
   <!--include_once('header.php'); -->
   <div class="main">
     <form method="post" action="" >  
       <h1>Se connecter</h1>    
       <div class="inputs">
-        <label for="email"></label>
+        
         <input type="email" name="login" placeholder="email">
-        <label for="password"></label>
+        
         <input type="password" name="mdp" placeholder="pass">
         <input type="submit" name="formconnexion" value="Se connecter !" />
       </div>
@@ -58,3 +42,19 @@ if(isset($_POST['formconnexion'])) {
 </html>
 
 
+
+
+
+<?php
+//   if(!empty($email)) {
+//     $recherchemail = $mysqlConnection->prepare("SELECT * FROM utilisateur WHERE email= ? ");
+//     $recherchemail ->execute(array($email));
+//     $userexist = $recherchemail->rowCount();
+//     if($userexist == 1) {
+//      echo "concordance";
+//     }else{
+//       echo " non concordance";
+//     }
+//   }
+
+?>
